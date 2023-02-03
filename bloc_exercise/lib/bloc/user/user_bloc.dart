395e6c25d6130d1831e024be3d50b09846352106
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2023-02-03 08:22:25 +0700

    initial state

--- bloc_exercise/lib/bloc/user/user_bloc.dart
+++ bloc_exercise/lib/bloc/user/user_bloc.dart
@@ -1,11 +1,14 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../models/user.dart';
 
 part 'user_event.dart';
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
+  UserBloc() : super(UserInitial([])) {
     on<UserEvent>((event, emit) {
       // TODO: implement event handler
     });
--- bloc_exercise/lib/pages/home/home.dart
+++ bloc_exercise/lib/pages/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../bloc/export.dart';
+import '../../models/user.dart';
 import '../add/add.dart';
 import '../edit/edit.dart';
 
@@ -8,27 +10,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    UserBloc userB = context.read<UserBloc>();
     return Scaffold(
       appBar: AppBar(
         title: const Text("ALL USER"),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => const EditPage()));
+      body: BlocBuilder<UserBloc, UserState>(
+        bloc: userB,
+        builder: (context, state) {
+          if (state.allUsers.isEmpty) {
+            return const Center(
+              child: Text("TIDAK ADA DATA"),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              User user = state.allUsers[index];
+              return ListTile(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const EditPage()));
+                },
+                leading: CircleAvatar(
+                  child: Text("${index + 1}"),
+                ),
+                title: Text(user.name),
+                subtitle: Text("${user.age} tahun"),
+                trailing: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.delete)),
+              );
             },
-            leading: CircleAvatar(
-              child: Text("${index + 1}"),
-            ),
-            title: Text("Nama user"),
-            subtitle: Text("Umur user"),
-            trailing:
-                IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+            itemCount: state.allUsers.length,
           );
         },
-        itemCount: 10,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
